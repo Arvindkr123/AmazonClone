e491176ac3e2d7c49fa6a19b87d05fe0502a8f38
Arvind <thakurarvindkr10@gmail.com>
2023-06-13 09:29:58 +0530

    added layer and install mui icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import { logo } from '../../assets'
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 const Header = () => {
     return (
-        <div className='w-full bg-amazon_blue text-white px-4 py-3'>
-            Header
+        <div>
+            <div className='w-full bg-amazon_blue text-white px-4 py-3 flex items-center gap-3'>
+                {/* ------------------      IMAGE STARTS HERE -------------------- */}
+                <div className='headerHover'>
+                    <img src={logo} className='w-24 mt-2' alt="logo" />
+                </div>
+                {/* Deliver start here */}
+                <div className='headerHover'>
+                    <LocationOnOutlinedIcon />
+                    <p className='text-sm text-lightText font-light flex flex-col '>Deliver to {""}
+                        <span className='text-sm font-semibold -mt-1 text-whiteText'>Delhi India</span>
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
