17f407c8bc21ae6fab9f54bf60d1341284300fea
Arvind <thakurarvindkr10@gmail.com>
2023-06-13 11:17:19 +0530

    headerTop done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,6 +3,7 @@ import { logo } from '../../assets'
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 import ArrowDropDownOutlinedIcon from '@mui/icons-material/ArrowDropDownOutlined';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import { allItems } from '../../constants';
 
 
@@ -45,6 +46,27 @@ const Header = () => {
                         <SearchOutlinedIcon />
                     </span>
                 </div>
+                {/* Search Ends here */}
+                {/* SignIn starts here */}
+                <div className='flex flex-col items-start justify-center headerHover'>
+                    <p className='text-xs text-lightText font-light'>Hello, sign in</p>
+                    <p className='text-sm font-semibold -mt-1 text-whiteText'>Accounts & Lists <span><ArrowDropDownOutlinedIcon /></span></p>
+                </div>
+                {/* SignIn end here */}
+                {/* Orders starts here  */}
+                <div className="flex flex-col items-start justify-center headerHover">
+                    <p className='text-xs text-lightText font-light'>Returns</p>
+                    <p className='text-sm font-semibold -mt-1 text-whiteText'>& Orders</p>
+                </div>
+                {/* Orders ends here  */}
+                {/* Cart Start here */}
+                <div className='flex items-start justify-center headerHover relative'>
+                    <ShoppingCartOutlinedIcon />
+                    <p className="text-xs font-semibold mt-3 text-whiteText">
+                        Cart <span className='absolute text-xs -top-1 left-6 font-semibold p-1 h-4 bg-[#f3a847] text-amazon_blue rounded-full flex justify-center items-center'>0</span>
+                    </p>
+                </div>
+                {/* Cart End here */}
             </div>
         </div>
     )
