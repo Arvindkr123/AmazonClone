68f9544d8be8ea142a77185177c478aae3cda448
Arvind <thakurarvindkr10@gmail.com>
2023-06-13 08:52:35 +0530

    add custom setup in

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,23 @@ export default {
         lg: "960px",
         lgl: "1024px",
         xl: "1280px"
+      },
+      fontFamily: {
+        titleFont: 'Roboto',
+        bodyFont: "Poppins"
+      },
+      colors: {
+        amazon_blue: "#131921",
+        amazon_ligh: "#232F3E",
+        amazon_yellow: "#febd69",
+        whiteText: "#ffffff",
+        lightText: "#ccc",
+        quantity_box: "#ccc",
+        footerBottom: "#131A22",
+      },
+      boxShadow: {
+        textShadow: "0px 0px 32px 1px rgba(199,199, 199,1)",
+        amazonInput:"0 0 3px 2px rgba(228, 121, 17/50%)"
       }
     },
   },
